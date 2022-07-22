7c079da581638181fd408e79ef1109030c5ae291
Prajwal Maske <57887193+illiterated@users.noreply.github.com>
2022-07-23 01:20:51 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Prajwalll !! Hi , hello is the string needed here.Bye";
+    return "Hello world- This is Prajwalll webhoook !! Hi , hello is the string needed here.Bye";
   }
 }
